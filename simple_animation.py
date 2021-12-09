7c4e374060840f30c75519e1d45cf09c3abf1a4d
il0milotrin <90473247+il0milotrin@users.noreply.github.com>
2021-12-09 14:32:35 -0500

    v0.2
    Established directions for DIAGONAL movement.

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with the PyGame, Trinity Gibbs, 12/9/21, 2:20pm, v0.1
+# Simple Animation with the PyGame, Trinity Gibbs, 12/9/21, 2:30pm, v0.2
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWWEIGHT == 400
 windowSurface = pygame.display.set_mode(WINDOWWIDTH, WINDOWHHEIGHT), 0,32)
 pygame.display.set_caption('Animation Example!') 
+
+# Setup the direction variables. 
+DOWNLEFT =  'downleft'
+DOWNRIGHT = 'downright' 
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
