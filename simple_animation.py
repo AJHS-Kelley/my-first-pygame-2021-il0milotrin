8ffe964af473bc90a9bbc8a5b9fb7a81cebcb146
il0milotrin <90473247+il0milotrin@users.noreply.github.com>
2021-12-09 14:23:50 -0500

    v0.1
    Imported models for PyGame, system, and time.
    
    Initialized the PyGame model.
    
    Setup window dimensions.

--- simple_animation.py
+++ simple_animation.py
@@ -1,1 +1,13 @@
-# Simple Animation with the PyGame, Trinity Gibbs, 12/9/21, 2:05pm, v0.0
+# Simple Animation with the PyGame, Trinity Gibbs, 12/9/21, 2:20pm, v0.1
+
+import pygame, sys, time
+from pygame.locals import *
+
+# Setup PyGame
+pygame.init() 
+
+# Setup the window
+WINDOWWIDTH = 400
+WINDOWWEIGHT == 400
+windowSurface = pygame.display.set_mode(WINDOWWIDTH, WINDOWHHEIGHT), 0,32)
+pygame.display.set_caption('Animation Example!') 
